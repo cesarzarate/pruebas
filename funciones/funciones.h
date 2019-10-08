76168b3fa746b1bb1929f14f4a125cd9a5efb18c
cesarzarate <38362143+cesarzarate@users.noreply.github.com>
2019-10-08 14:00:42 -0500

    Add files via upload

---
+++ funciones/funciones.h
@@ -0,0 +1,25 @@
+int sumar(int a, int b);
+int restar(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+
+int sumar(int a, int b){
+	int res = a + b;
+	return res;
+}
+
+int restar(int a, int b){
+	int res = a - b;
+	return res;
+}
+
+int mul(int a, int b){
+	int res = a * b;
+	return res;
+}
+
+int div(int a, int b){
+	int res = a / b;
+	return res;
+}
+
